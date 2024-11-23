fdba9ef403c554c44f39e71e97711f528d806451
Lulu969 <163893010+Lulu969@users.noreply.github.com>
2024-11-23 03:02:44 -0400

    Add files via upload

---
+++ animacion.m
@@ -0,0 +1,31 @@
+function [] = animacion()
+  n=30;
+  for i = 1:n
+    esc1();
+    pause(.1);
+    esc2();
+    pause(.1);
+    esc1();
+    pause(.1);
+    esc2();
+    pause(.1);
+    esc3();
+    pause(.1);
+    esc4();
+    pause(.1);
+    esc5();
+    pause(.1);
+    esc6();
+    pause(.1);
+    esc7();
+    pause(.1);
+    esc8();
+    pause(.1);
+    esc9();
+    pause(.1);
+    esc10();
+    pause(.1);
+    esc11();
+    pause(.1);
+  endfor
+end
